8cc0e4e91ca5f7b1709607885df85c2c560809fb
Michael Crowell <Michael Crowell>
2016-06-21 13:27:34 -0400

    Very basic prototype for end-to-end is complete.

--- Interfacing Script/testSinatra.rb
+++ Interfacing Script/testSinatra.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/ruby
+require 'sinatra'
 require 'mysql2'
 require 'erb'
 
@@ -7,4 +8,11 @@ template_file = File.read(template_path)
 
 # Interpret erb file
 html_doc = ERB.new(template_file).result(binding)
-puts html_doc
+
+get '/' do
+	html_doc
+end
+
+get '/about' do
+  'A little about me.'
+end
